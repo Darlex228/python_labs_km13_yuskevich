285697aa6c54d480fe130ac5b522290fd412b4d6
Eduardy <91112377+Darlex228@users.noreply.github.com>
2021-10-25 13:46:39 +0300

    Update p7_yuskevich_1.py

--- p7_yuskevich/p7_yuskevich_1.py
+++ p7_yuskevich/p7_yuskevich_1.py
@@ -1,2 +1,2 @@
-format_tuple = ('Liverpool', 845.15, 21, 134, 358.45, 'price')
-print('The initial lot {} of ${} at the {} auction exceeded the expected {} by {}%, but the lot with number {} was nevertheless sold for ${}.'.format(format_tuple[5], round(format_tuple[4], 1), format_tuple[0], format_tuple[5], format_tuple[2], str(format_tuple[3]).zfill(4), int(format_tuple[1])))
+f_tpl = ('Liverpool', 845.15, 21, 134, 358.45, 'price')
+print('The initial lot {} of ${} at the {} auction exceeded the expected {} by {}%, but the lot with number {} was nevertheless sold for ${}.'.format(f_tpl[5], round(f_tpl[4], 1), f_tpl[0], f_tpl[5], f_tpl[2], str(f_tpl[3]).zfill(4), int(f_tpl[1])))
