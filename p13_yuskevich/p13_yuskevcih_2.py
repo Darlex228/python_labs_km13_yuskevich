8387ad927680a81dfc999898cfa4b8bbf9a4d683
Eduardy <91112377+Darlex228@users.noreply.github.com>
2021-12-04 02:57:12 +0200

    Update p13_yuskevcih_2.py

--- p13_yuskevich/p13_yuskevcih_2.py
+++ p13_yuskevich/p13_yuskevcih_2.py
@@ -1,11 +1,11 @@
-M = {}
-F = {}
+m = {}
+fm = {}
 
 for i in range(1880, 2020):
     s = 'yob' + str(i) + '.txt'
     f = open(s, 'r')
-    max_M = 0
-    max_F = 0
+    max_m = 0
+    max_fm = 0
 
     for j in f:
         j = j.split(',')
@@ -13,25 +13,25 @@ for i in range(1880, 2020):
         sex = j[1]
         amount = int(j[2])
         if sex == 'M':
-            max_M = max(max_M, amount)
+            max_m = max(max_m, amount)
         if sex == 'F':
-            max_F = max(max_F, amount)
+            max_fm = max(max_fm, amount)
     f = open(s, 'r')
     for u in f:
         u = u.split(',')
         name = u[0]
         sex = u[1]
         amount = int(u[2])
-        if amount == max_M and sex == 'M':
-            M[name] = M.get(name, 0) + 1
+        if amount == max_m and sex == 'M':
+            m[name] = m.get(name, 0) + 1
 
-        if amount == max_F and sex == 'F':
-            F[name] = F.get(name, 0) + 1
+        if amount == max_fm and sex == 'F':
+            fm[name] = fm.get(name, 0) + 1
     f.close()
-M = sorted(M.items(), key=lambda h: h[1], reverse=True)
-F = sorted(F.items(), key=lambda h: h[1], reverse=True)
-for i in M:
+m = sorted(m.items(), key=lambda h: h[1], reverse=True)
+fm = sorted(fm.items(), key=lambda h: h[1], reverse=True)
+for i in m:
     print(i[0], i[1])
 print('-------------------------------------')
-for i in F:
+for i in fm:
     print(i[0], i[1])
